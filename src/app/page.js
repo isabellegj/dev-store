2f1c3015d96a64a70e8f1f4dec236ffed1afd400
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-07-03 17:48:56 -0300

    modified home pago to keep products in cache for 2 minutes

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,34 @@
 "use client";
 import Header from "../components/header";
 import Card from "../components/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useProducts from "../hooks/useProducts";
 
+const CACHE_DURATION = 2 * 60 * 1000;
+
 export default function Home() {
-  const products = useProducts();
+  const allProducts = useProducts();
+  const [cachedProducts, setCachedProducts] = useState([]);
+  const [lastFetchTime, setLastFetchTime] = useState(0);
   const [clickCount, setClickCount] = useState(0);
 
-  const handleClick = () => {
-    setClickCount((prevCount) => prevCount + 1);
-  };
+  useEffect(() => {
+    if (
+      Date.now() - lastFetchTime < CACHE_DURATION &&
+      cachedProducts.length > 0
+    ) {
+      return;
+    }
+
+    setCachedProducts(allProducts);
+    setLastFetchTime(Date.now());
+  }, [allProducts, lastFetchTime, cachedProducts]);
 
   return (
     <div className="min-h-screen bg-gray">
       <Header count={clickCount} />
       <div className="grid md:grid-cols-[repeat(4,1fr)] grid-rows-[repeat(x,1fr)] lg:px-40 py-10 justify-center">
-        {products.map((product) => (
+        {cachedProducts.map((product) => (
           <Card key={product.id} product={product} />
         ))}
       </div>
